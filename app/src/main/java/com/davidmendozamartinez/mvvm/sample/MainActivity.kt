2fc171c72b33c11edc4139e2c4cd48952c1a5c19
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-03-30 20:09:53 +0200

    Migrate MainPresenter to MainViewModel without DataBinding

--- app/src/main/java/com/davidmendozamartinez/mvvm/sample/MainActivity.kt
+++ app/src/main/java/com/davidmendozamartinez/mvvm/sample/MainActivity.kt
@@ -3,12 +3,14 @@ package com.davidmendozamartinez.mvvm.sample
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.davidmendozamartinez.mvvm.sample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), MainPresenter.View {
+class MainActivity : AppCompatActivity() {
 
-    private val presenter = MainPresenter(this, lifecycleScope)
+    private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,18 +18,18 @@ class MainActivity : AppCompatActivity(), MainPresenter.View {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel = ViewModelProvider(this).get()
+        viewModel.progressVisibility.observe(this, Observer { visible ->
+            binding.progress.visibility = if (visible) View.VISIBLE else View.GONE
+        })
+        viewModel.message.observe(this, Observer { message ->
+            binding.message.text = message
+        })
+
         with(binding) {
             button.setOnClickListener {
-                presenter.onButtonClicked(user.text.toString(), pass.text.toString())
+                viewModel.onButtonClicked(user.text.toString(), pass.text.toString())
             }
         }
-    }
-
-    override fun setProgressVisible(visible: Boolean) {
-        binding.progress.visibility = if (visible) View.VISIBLE else View.GONE
-    }
-
-    override fun setMessage(message: String) {
-        binding.message.text = message
     }
 }
